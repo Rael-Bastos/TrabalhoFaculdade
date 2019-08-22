44037ea54cf152f3af6e69fafb9c4b72535e7c35
israel.jesus <israel.jesus@FULL-TEC-0064>
2019-08-22 11:12:32 -0300

    Adicionar arquivos de projeto.

---
+++ NoticiasFmuTrabalho/Controllers/NoticiasController.cs
@@ -0,0 +1,29 @@
+﻿using NoticiasFmuTrabalho.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NoticiasFmuTrabalho.Controllers
+{
+    public class NoticiasController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View(RepositorioNoticias.Noticias);
+        }
+        public ActionResult Detalhes(int id)
+        {
+            var noticia = RepositorioNoticias.Noticias.FirstOrDefault(x => x.Id == id);
+            return View(noticia);
+        }
+
+        public ActionResult Busca(string texto)
+        {
+            var buscarNoticias = RepositorioNoticias.Noticias.Where(x => x.Titulo.Contains(texto) || x.Conteudo.Contains(texto));
+            return View(buscarNoticias);
+        }
+    }
+}
---
+++ NoticiasFmuTrabalho/Models/RepositorioNoticias.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NoticiasFmuTrabalho.Models
+{
+    public class RepositorioNoticias
+    {
+        private static List<Noticias> noticias;
+
+        public static List<Noticias> Noticias
+        {
+            get
+            {
+                if (noticias == null)
+                    CriarNoticias();
+                return noticias;
+            }
+        }
+        private static void CriarNoticias()
+        {
+            noticias = new List<Noticias>();
+
+            noticias.Add(new Noticias
+            {
+                Id = 1,
+                Titulo = " Teste 1",
+                Autor = "Israel Bastos",
+                Data = DateTime.Today,
+                Conteudo = "Teste 1, Teste 1,Teste 1, Teste 1,Teste 1"
+            });
+
+            noticias.Add(new Noticias
+            {
+                Id = 2,
+                Titulo = " Teste 2",
+                Autor = "Israel Basto 2",
+                Data = DateTime.Today,
+                Conteudo = "Teste 2, Teste 2,Teste 2, Teste 2,Teste 2"
+            });
+
+            noticias.Add(new Noticias
+            {
+                Id = 3,
+                Titulo = " Teste 3",
+                Autor = "Israel Basto 3",
+                Data = DateTime.Today,
+                Conteudo = "Teste 3, Teste 3,Teste 3, Teste 3,Teste 3"
+            });
+
+        }
+    }
+}
